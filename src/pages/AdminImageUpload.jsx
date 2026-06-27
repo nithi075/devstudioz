f6b200ef62e19323b85136b1d1669e0c2b9de58e
Sam Nithish <nithishraj8055@gmail.com>
2026-06-27 13:27:21 +0530

    Fix import path for imageUpload.css

--- src/pages/AdminImageUpload.jsx
+++ src/pages/AdminImageUpload.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
-import "./imageUpload.css";
+import "./ImageUpload.css";
 
 export default function AdminImageUpload() {
   const [title, setTitle] = useState("");
